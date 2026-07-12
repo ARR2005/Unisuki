9e1aa8a5b72dd3393f9eb6c8abaf721b3c7b5621
ARR2005 <Aldwinrich90@gmail.com>
2026-07-12 23:29:35 +0800

    styles:  Reanimated the custom Navbar

--- components/CustomNavbar.tsx
+++ components/CustomNavbar.tsx
@@ -1,11 +1,20 @@
-import { View, TouchableOpacity, Text} from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useTheme } from "@react-navigation/native";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs"
+import { Ionicons } from '@expo/vector-icons';
+import Animated, { FadeIn, LinearTransition } from 'react-native-reanimated';
+
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+
+
+const PrimaryColor = "#4CAF50"; // Example primary color    
+const SecondaryColor = "#FFFFFF"; // Example secondary color
+
 
 const CustomNavBar: React.FC<BottomTabBarProps> = ({ state, descriptors, navigation }) => {
   const { colors } = useTheme();
   return (
-    <View style={{ flexDirection: 'row' }}>
+    <View style={ styles.container }>
       {state.routes.map((route, index) => {
         console.log("route", route);
 
@@ -32,23 +41,75 @@ const CustomNavBar: React.FC<BottomTabBarProps> = ({ state, descriptors, navigat
         };
 
         return (
-          <TouchableOpacity
+            <AnimatedTouchableOpacity
+             layout={LinearTransition.springify().mass(0.5)}
             key={route.key}
             onPress={onPress}
-            style={{ flex: 1 }}
+            style={ [styles.tabItem , {backgroundColor: isFocused ? PrimaryColor : "transparent"} ] }
           >
-            <Text style={{ color: isFocused ? colors.primary : colors.text }}>
+            {getIconByRouteName(route.name, "#fff")}
+            {isFocused && <Animated.Text entering={FadeIn.duration(200) } style={ styles.text }>
               {label as string}
-            </Text>
-          </TouchableOpacity>
+            </Text>}
+          </AnimatedTouchableOpacity>
         );
       })}
     </View>
   );
 }
 
-const styles = {
-    
-};
+    function getIconByRouteName(routeName: string, colors: string) {
+      switch (routeName) {
+        case 'home': 
+            return <Ionicons name="home-outline" size={28} color={colors} />
+        case 'message':
+            return <Ionicons name="chatbubble-outline" size={28} color={colors} />
+        case 'camera':
+            return <Ionicons name="add-circle-outline" size={28} color={colors} />
+        case 'reserve': 
+            return <Ionicons name="calendar-outline" size={28} color={colors} />
+        case 'profile': 
+            return <Ionicons name="person-outline" size={28} color={colors} />
+        default:
+            return null;
+      }
+
+
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        bottom: 30,
+        
+        alignSelf: 'center',
+        flexDirection: 'row',
+        
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        
+        width: '95%' ,
+        
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
+        borderRadius: 50,
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+    },
+    tabItem: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        borderRadius: 50,
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 16,
+        marginLeft: 6,
+    },
+})
 
 export default CustomNavBar;
